cf8f0e3a387df51c7be618aec567049e1338079c
Priyangshu Dhar <priyangshudhar700@gmail.com>
2022-03-31 22:27:08 +0530

    Thread-pack

--- thread/Thread_method.java
+++ thread/Thread_method.java
@@ -1,23 +1,45 @@
 package thread;
 class Thr extends Thread
 {
-	public void run()
+	public synchronized void run()
 	{
+		//System.out.println(this.interrupted());
+		//System.out.println(this.isInterrupted());
+		try {
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i+" "+this.getName());
+			//System.out.println(this.interrupted());
+			//System.out.println(this.isInterrupted());
+				//this.sleep(1000);
+				
+		}
 		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	}
-class Thre extends Thread
-{
-	public void run()
-	{
-		
-	}
-}
+//class Thre extends Thread
+//{
+//	public void run()
+//	{
+//		
+//	}
+//}
 
 public class Thread_method {
 
+	
 	public static void main(String[] args) {
-	System.out.println("hello");
+		
+		Thr t= new Thr();
+		t.start();
+		//t.interrupt();
+		Thr t1= new Thr();
+		t1.start();
 	}
 
 }
